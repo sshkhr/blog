3c6207fdad9926e609c053c16ee2cd9599808d93
Shashank Shekhar <contactshashankshekhar@gmail.com>
2024-05-09 16:14:15 -0400

    UI Change: Renamed publication to research

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
-  { href: '/publications', title: 'Publications' },
+  { href: '/research', title: 'Research' },
   { href: '/about', title: 'About' },
 ]
 
