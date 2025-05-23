1956cd37b311a7dd75f089fb38bd4e39e56003ce
Shashank Shekhar <contactshashankshekhar@gmail.com>
2025-05-23 01:05:38 -0400

    add data quality blog

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -63,7 +63,7 @@ const computedFields: ComputedFields = {
 function createTagCount(allBlogs) {
   const tagCount: Record<string, number> = {}
   allBlogs.forEach((file) => {
-    if (file.tags && (!isProduction || file.draft !== true)) {
+    if (file.tags && !isProduction) {
       file.tags.forEach((tag) => {
         const formattedTag = slug(tag)
         if (formattedTag in tagCount) {
